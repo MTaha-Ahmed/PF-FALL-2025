4bcd052b14c6915d03d882a8a479b280b93d3bd8
Taha <k250650@nu.edu.pk>
2025-09-29 10:39:33 +0500

    work

--- Untitled1.c.cpp
+++ Untitled1.c.cpp
@@ -2,20 +2,21 @@
 #include <string.h>
 int main()
 {
+	int i=1;
 	char Name[20] = "user";
 	char pass[20] = "1234";
 	char uname[20],upass[20];
-	int flag;
+	int flag=0;
 	do{
 		printf("Username: \n");
 		scanf("%s",uname);
 		printf("Pass: \n");
 		scanf("%s",upass);
 		if(strcmp(pass,upass)==0 && strcmp(Name,uname)==0){
-		flag =1;
 		printf("Welcome");
+		flag =1;
 	} else
-	printf("Try Again\n");
+	printf("Try Again \n");
 	}while(!flag);
 	return 0;
 }
---
+++ Untitlede.c
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+	int day;
+	printf("Enter day(1-7): \n");
+	scanf("%d",&day);
+	switch(day){
+		case 1:
+			printf("Monday");
+	}
+}
